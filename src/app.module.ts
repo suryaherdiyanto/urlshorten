38af666f382e5af41e5043354bd3412257e64834
suryaherdiyanto <suryaherdiyanto@gmail.com>
2023-07-30 15:45:35 +0800

    adding nestjs/config & setup the .env file for test and dev environtment

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+      isGlobal: true
+    }),
     TypeOrmModule.forRoot({
       database: 'db.sqlite',
       type: 'sqlite',
       entities: [`${__dirname}/*/**.entities.ts`]
-    })
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
