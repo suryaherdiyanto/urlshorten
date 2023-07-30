77186c56d132974ef3344bf56b69590b4e05993c
suryaherdiyanto <suryaherdiyanto@gmail.com>
2023-07-30 15:16:53 +0800

    setup a session storage

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
+
+
+  app.use(session({
+    secret: 'the-example-secret',
+    saveUninitialized: false,
+    resave: false
+  }));
   await app.listen(3000);
 }
 bootstrap();
