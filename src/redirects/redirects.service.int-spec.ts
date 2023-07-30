2dc170aab7490f3810a576c37146f74197fb06d5
suryaherdiyanto <suryaherdiyanto@gmail.com>
2023-07-30 19:32:06 +0800

    adding test cases for findByReferrer and create methods for redirectsService, and also implement the implementation

--- src/redirects/redirects.service.int-spec.ts
+++ src/redirects/redirects.service.int-spec.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule, getRepositoryToken } from '@nestjs/typeorm';
 import { Redirect } from './entities/redirects.entity';
 import { RedirectsModule } from './redirects.module';
 import { Repository } from 'typeorm';
+import { CreateRedirectDto } from './dto/create-redirect.dto';
 
 describe('RedirectsService', () => {
   let service: RedirectsService;
@@ -25,6 +26,8 @@ describe('RedirectsService', () => {
 
     service = module.get<RedirectsService>(RedirectsService);
     repository = module.get(getRepositoryToken(Redirect));
+
+    await repository.createQueryBuilder().insert().into(Redirect).values([{ referrer: 'qweRTy', full_url: 'https://www.facebook.com/john.doe'}]).execute();
   });
 
   it('service should be defined', () => {
@@ -35,6 +38,23 @@ describe('RedirectsService', () => {
   });
 
   describe('findByReferrer', function() {
-
+    it('should find record from database by the referrer and return the correct full_url', async function() {
+      expect((await service.findByReferrer('qweRTy')).full_url).toEqual('https://www.facebook.com/john.doe');
+    });
+    it('should return null if we don\' find matched record', async function() {
+      expect((await service.findByReferrer('dummy'))).toEqual(null);
+    });
   });
+
+  describe('create', function() {
+    const dto: CreateRedirectDto = { full_url: 'https://www.youtube.com/john-doe' };
+    it('should create a new record to database of Redirect entity', async function() {
+      expect((await service.create(dto.full_url))).toEqual({
+        id: expect.any(Number),
+        full_url: dto.full_url,
+        hit_counts: 0,
+        referrer: expect.any(String)
+      });
+    });
+  })
 });
--- src/redirects/redirects.service.ts
+++ src/redirects/redirects.service.ts
@@ -6,4 +6,15 @@ import { Redirect } from './entities/redirects.entity';
 @Injectable()
 export class RedirectsService {
     constructor(@InjectRepository(Redirect) private repository: Repository<Redirect>) {}
+
+    findByReferrer(referrer: string) {
+        return this.repository.findOneBy({ referrer });
+    }
+
+    create(full_url: string) {
+        const referrer = 'a';
+        const redirect = this.repository.create({ full_url, referrer });
+
+        return this.repository.save(redirect);
+    }
 }
