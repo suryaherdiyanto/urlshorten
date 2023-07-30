32bee17a02118d5bd29002bc837d485fcf50fe5d
suryaherdiyanto <suryaherdiyanto@gmail.com>
2023-07-30 15:26:07 +0800

    adding sqlite3 and setup typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      database: 'db.sqlite',
+      type: 'sqlite',
+      entities: [`${__dirname}/*/**.entities.ts`]
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,11 @@ import * as session from 'express-session';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  // Setup view engine
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
 
-
+  // Setup the session storage
   app.use(session({
     secret: 'the-example-secret',
     saveUninitialized: false,
