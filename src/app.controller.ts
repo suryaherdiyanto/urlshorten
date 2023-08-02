1c115da363f48fe8853772618df90ad4611387e0
suryaherdiyanto <suryaherdiyanto@gmail.com>
2023-08-02 19:17:47 +0800

    add: decorator for flash message

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,26 +1,29 @@
-import { Body, Controller, Get, Post, Render, Res } from '@nestjs/common';
+import { Body, Controller, Get, Post, Render, Req, Res, Session } from '@nestjs/common';
 import { AppService } from './app.service';
 import { CreateCsrfToken } from './decorators/csrf.decorator';
 import { Reflector } from '@nestjs/core';
 import { RedirectsService } from './redirects/redirects.service';
 import { CreateRedirectDto } from './redirects/dto/create-redirect.dto';
-import { Response } from 'express';
+import { Request, Response } from 'express';
+import { ConfigService } from '@nestjs/config';
+import { FlashMessage } from './decorators/flash-message.decorator';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService, private readonly reflector: Reflector, private readonly redirectService: RedirectsService) {}
+  constructor(private readonly appService: AppService, private readonly reflector: Reflector, private readonly redirectService: RedirectsService, private readonly config: ConfigService) {}
 
   @Get()
   @Render('index')
   @CreateCsrfToken()
-  getHello(): object {
-    return { hello: this.appService.getHello(), csrf: this.reflector.get<string>('csrf', this.getHello)};
+  getHello(@FlashMessage('success') successMessage: string|null, @FlashMessage('data') data: string|null): object {
+    return { hello: this.appService.getHello(), flash: { successMessage, data }, csrf: this.reflector.get<string>('csrf', this.getHello)};
   }
 
   @Post('/')
-  async shortenUrl(@Body() data: CreateRedirectDto, @Res() res: Response) {
+  async shortenUrl(@Body() data: CreateRedirectDto, @Session() session: any, @Res() res: Response) {
     const { referrer } = await this.redirectService.create(data.full_url);
-    console.log(referrer);
+    session.flash['success'] = 'Successfully create a shortern URL';
+    session.flash['data'] = `Here is your shorten URL:\n${this.config.get<string>('APP_URL')}/s/${referrer}`;
 
     res.redirect('/');
   }
