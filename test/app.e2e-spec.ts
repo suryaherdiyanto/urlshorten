5ffdfb0e769d91e7832839710f1a389e828b500d
suryaherdiyanto <suryaherdiyanto@gmail.com>
2023-08-05 22:17:37 +0800

    setup e2e testing

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,47 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { HttpException, INestApplication, ValidationPipe, HttpStatus, ValidationError } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as cookieParser from 'cookie-parser';
+import * as session from 'express-session';
+import { join } from 'path';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication;
+  let app: NestExpressApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+    app = moduleFixture.createNestApplication<NestExpressApplication>();
+    // Setup view engine
+    app.setBaseViewsDir(join(__dirname, '..', 'views'));
+    app.setViewEngine('ejs');
+
+    // Cookie
+    app.use(cookieParser('some-cookie-secret'));
+
+    // Setup the session storage
+    app.use(session({
+      secret: 'the-example-secret',
+      saveUninitialized: false,
+      resave: false
+    }));
+
+
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true, validationError: { value: false }, exceptionFactory: (errors: ValidationError[]) => {
+      const validationErrors = errors.map((error) => { return { field: error.property, errorMessages: Object.values(error.constraints) } });
+
+      return new HttpException({ message: 'Invalid input', validationErrors }, HttpStatus.BAD_REQUEST);
+    } }));
+      await app.init();
+    });
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
       .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .expect(200);
   });
 });
